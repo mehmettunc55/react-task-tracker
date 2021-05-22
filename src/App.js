c35709b93fb96be4a87b2694d842e265afae359e
mehmettunc55 <mtunc551980@gmail.com>
2021-05-22 18:30:11 +0300

    tasks created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import {useState} from 'react'
 import Header from './components/Header';
 import './App.css';
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Study React Pre-Class Notes',
+      day: 'Feb 5th at 2:30pm',
+      isDone: false,
+    },
+    {
+      id: 2,
+      text: 'Feed the Dog',
+      day: 'Feb 6th at 1:30pm',
+      isDone: false,
+    },
+    {
+      id: 3,
+      text: 'Attend in-Class',
+      day: 'Feb 7th at 20:00pm',
+      isDone: false,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header title = 'Task Tracker'/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Task from './Task';
+
+
+const Tasks = ({tasks}) => {
+
+    
+
+
+    return (
+        <div>
+            {tasks?.map((task) => (
+               <Task key={task.id} task={task}/>
+            ))}
+        </div>
+    )
+}
+
+export default Tasks
